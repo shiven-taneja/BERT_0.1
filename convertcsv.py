bfb716a4fe29433cf2ce741f4d2b68c906b993ba
Shiven Taneja <shiven.taneja@gmail.com>
2023-03-25 20:59:38 -0400

    Ran 4 epochs for wiki-text103 and CHILDES

--- convertcsv.py
+++ convertcsv.py
@@ -1,11 +1,10 @@
 import csv, os, sys
 import re
 
-with open('train_output.csv', 'w', newline="", encoding="utf-8") as out_file:
+with open('train_output_child.csv', 'w', newline="", encoding="utf-8") as out_file:
     csv_out = csv.writer(out_file)
-    csv_out.writerow(['filename', 'Paragraphs'])
-    with open('wiki.test.tokens', 'r', encoding='utf-8') as txt:
-        for line in txt.read().splitlines():
-            if re.search("^[a-zA-Z]", line.strip()) is not None:
-                csv_out.writerow(['test_tokens_file', line])
+    csv_out.writerow(['filename', 'Transcript'])
+    with open('childes.txt', 'r') as txt:
+        as_string = " ".join(line.strip() for line in txt)
+        csv_out.writerow(['childes_data', as_string])
     csv_out.close()
